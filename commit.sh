bfa4bf66dd673188db77e00f37f27f6adbe8d080
Alan <alan@remote.com>
2020-08-19 19:42:27 -0500

    test commit.sh

--- commit.sh
+++ commit.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-for i in "$*"
-do 
-    echo $i
-done
+# usage 
+echo "enter pull branch > "
+read PULL_BRANCH
 
-git pull origin master
-git add .
-git commit -m "$*"
-# git push
+echo "files > "
+read FILES
+
+echo "commit message > "
+read MSG
+git pull origin $PULL_BRANCH
+git add $FILES
+git commit -m "$MSG"
+git push
