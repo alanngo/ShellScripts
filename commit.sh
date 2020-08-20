b421e19c4e0de4e8e3e892741ba7db73597817c6
Alan <alan@remote.com>
2020-08-19 21:02:25 -0500

    6627 loves 74355

--- commit.sh
+++ commit.sh
@@ -1,13 +1,40 @@
 #!/bin/sh
 # usage: ./commit.sh <PULL_BRANCH> <PUSH_BRANCH>
 
+# echo "files > "
+# read FILES
+
+# echo "commit message > "
+# read MSG
+
+# pull
+if [ -z "$1" ]; then
+    echo "DEFAULT: pulling from orign master"
+    git pull origin master
+else
+    echo "pulling from $1"
+    git pull origin $1
+fi
+
+# stage files
 echo "files > "
 read FILES
+git add $FILES
 
+#commit to local repository
 echo "commit message > "
 read MSG
-
-git pull origin $1
-git add $FILES
 git commit -m "$MSG"
-git push origin -u $2
+
+# push
+if [ -z "$2" ]; then
+    echo "DEFAULT: pushing to orign master"
+    git push origin -u origin master
+else
+    echo "pushing to $2"
+    git push origin -u $2
+fi
+# git pull origin $1
+# git add $FILES
+# git commit -m "$MSG"
+# git push origin -u $2
