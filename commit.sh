780a4458d0b62492a07f994d23dc2a7d1d3fbaf4
Alan <alan@remote.com>
2020-08-19 19:48:52 -0500

    testing default branch commits

--- commit.sh
+++ commit.sh
@@ -2,7 +2,10 @@
 # usage 
 echo "enter pull branch > "
 read PULL_BRANCH
-
+if [PULL_BRANCH =""]; then
+    echo "Default pull branch is master"
+    PULL_BRANCH="master"
+fi
 echo "files > "
 read FILES
 
@@ -11,6 +14,10 @@ read MSG
 
 echo "enter push branch > "
 read PUSH_BRANCH
+if [PUSH_BRANCH =""]; then
+    echo "Default pull branch is master"
+    PUSH_BRANCH="master"
+fi
 
 git pull origin $PULL_BRANCH
 git add $FILES
