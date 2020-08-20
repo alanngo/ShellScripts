7ccfd840ec86648ccf67259813e946624aba1adb
Alan <alan@remote.com>
2020-08-19 19:55:39 -0500

    cleaner way to commit

--- commit.sh
+++ commit.sh
@@ -1,25 +1,13 @@
 #!/bin/sh
 # usage 
-echo "enter pull branch > "
-read PULL_BRANCH
-if [$PULL_BRANCH =""]; then
-    echo "Default pull branch is master"
-    PULL_BRANCH="master"
-fi
+
 echo "files > "
 read FILES
 
 echo "commit message > "
 read MSG
 
-echo "enter push branch > "
-read PUSH_BRANCH
-if [$PUSH_BRANCH =""]; then
-    echo "Default pull branch is master"
-    PUSH_BRANCH="master"
-fi
-
-git pull origin $PULL_BRANCH
+git pull origin $1
 git add $FILES
 git commit -m "$MSG"
-git push origin $PUSH_BRANCH
+git push origin $2
