70ea44c1fe950bf0f41a695d1c24e0a7c18d248b
Alan <alan@remote.com>
2020-08-28 09:12:22 -0500

    meh

--- color.sh
+++ color.sh
@@ -3,6 +3,8 @@ GREEN='\u001b[32m'
 BLUE='\u001b[34m'
 YELLOW='\u001b[33m'
 WHITE='\u001b[37m'
+
+# use echo -e to print in certain colors
 echo -e ${RED} RED
 echo -e ${YELLOW} YELLOW
 echo -e ${GREEN} GREEN
