93f1ede3c6d07f067253fbd9ea7a689fe2925c85
Alan <alan@remote.com>
2020-08-27 23:12:21 -0500

    added colors to shell

--- commit.sh
+++ commit.sh
@@ -1,12 +1,15 @@
-#!/bin/sh
 # usage: ./commit.sh => push and pull from master
 #        ./commit.sh <PULL_BRANCH> <PUSH_BRANCH>
+
+INFO='\u001b[34m'
+SUCCESS='\u001b[32m'
+NORMAL='\u001b[37m'
 # pull
 if [ -z "$1" ]; then
-    echo "DEFAULT: pulling from orign master"
+    echo -e ${INFO} "DEFAULT: pulling from orign master"${NORMAL}
     git pull origin master
 else
-    echo "pulling from $1"
+    echo -e ${INFO} "pulling from $1"${NORMAL}
     git pull origin $1
 fi
 
@@ -24,9 +27,11 @@ git commit -m "$MSG"
 
 # push
 if [ -z "$2" ]; then
-    echo "DEFAULT: pushing to orign master"
+    echo -e ${INFO}"DEFAULT: pushing to orign master"${NORMAL}
     git push -u origin master
 else
-    echo "pushing to $2"
+    echo -e ${INFO}"pushing to $2"${NORMAL}
     git push origin -u $2
 fi
+
+echo -e ${SUCCESS}"Successfully pushed to remote"
