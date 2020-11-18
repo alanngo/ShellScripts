b4d626f50717b2b6dee70a0301764a3b877f6de2
Alan <alan@remote.com>
2020-11-18 15:30:12 -0600

    automation for creating expressjs projects

---
+++ automation/README.md
@@ -0,0 +1,21 @@
+# Automation w/ shell scripting
+
+## Features
+- c.sh: create a new C project
+- clone.sh: clone git repo
+- commit.sh: add, commit, push to a github repo
+- cors.sh: test for cross-origin capability for web app
+- cpp.sh: create a new C++ project
+- express.sh: create a new ExpressJS project
+- flask_init.sh: create a new flask project
+- java.sh: create a new java project
+- proc_kill.sh: kill multiple processes
+- py.sh: create a new python project
+- ts.sh: create a new typescript project
+
+## How to integrate w/ your workflow
+
+```bash
+$ cat {SH file} >> ~/{SH file}
+$ echo alias {ALIAS_NAME}='bash ~/{SH file}'
+```
---
+++ automation/express.sh
@@ -0,0 +1,57 @@
+RED='\u001b[31m'
+GREEN='\u001b[32m'
+BLUE='\u001b[34m'
+YELLOW='\u001b[33m'
+WHITE='\u001b[37m'
+
+if [ -z $1 ]; then
+    echo -e "${RED}Specify a project name!"
+    echo -e "${BLUE}Usage: $ bash express.sh <PROJ NAME>"
+    exit
+fi
+mkdir ${1}
+mkdir ${1}'/src'
+sudo npm install --prefix ${1} nodemon --save-dev && echo -e "${BLUE} installing nodemon"
+sudo npm install --prefix ${1} express --save && echo -e "${BLUE} installing express"
+sudo npm install --prefix ${1} cors --save && echo -e "${BLUE} installing cors"
+echo '{
+  "name"': '"'${1}'"'',
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "type":"module",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "nodemon src/index.js"
+  },
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "nodemon": "^2.0.6"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.17.1"
+  }
+}'>> ${1}'/package.json'
+
+echo 'import express from "express"
+import cors from "cors"
+const PORT = 3200
+const APP = express()
+
+// set up body parser
+const BODY_PARSER ={ extended: true }
+APP.use(cors()) 
+APP.use(express.json(BODY_PARSER))
+APP.use(express.urlencoded(BODY_PARSER))
+
+APP.get(`/`, (req, res) => 
+{
+    res.json({mssg: "spongebob is the best cartoon ever"})
+})
+
+APP.listen(PORT, () => console.log(`http://localhost:${PORT}`))' >>${1}'/src/index.js'
+
+echo -e ${GREEN}"To run your express app, use $ npm start"
+echo -e ${GREEN}"If you are having problems w/ running your app, use $ pkill -9 node && npm start"
