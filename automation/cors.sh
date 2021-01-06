97eb2f69100f007bb7c140f3a2ae2967c45efe08
Alan <alan@remote.com>
2021-01-06 17:06:29 -0600

    updated cors test to account for all requests

--- automation/cors.sh
+++ automation/cors.sh
@@ -1,6 +1,20 @@
-MY_URL=${1}
+function api()
+{
+  local URL=$1
+  local METHOD=$2
+  curl -I -X OPTIONS \
+    -H "Origin: ${MY_URL}" \
+    -H "Access-Control-Request-Method: ${METHOD}" \
+    "${MY_URL}" 2>&1 | grep -i 'Access-Control-Allow-Origin'
+}
 
-curl -I -X OPTIONS \
-  -H "Origin: ${MY_URL}" \
-  -H 'Access-Control-Request-Method: GET' \
-  "${MY_URL}/SOMETHING" 2>&1 | grep -i 'Access-Control-Allow-Origin'
+
+MY_URL=$1
+METHOD=$2
+if [ -z "$METHOD" ]; then
+  echo "default: using GET method"
+  api $MY_URL "GET"
+  exit
+fi
+  echo "using $METHOD method"
+  api $MY_URL $METHOD
