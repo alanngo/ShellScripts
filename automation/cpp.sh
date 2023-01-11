0a297dcec39ef0fc1edf1a33ad6f7f11e93d961c
alan <alanngo673@gmail.com>
2023-01-11 10:20:45 -0600

    console colors for c++

--- automation/cpp.sh
+++ automation/cpp.sh
@@ -10,19 +10,77 @@ MAIN="main.cpp"
 mkdir $PROJ_DIR
 echo '
 #include <iostream>
-
+#include "console.hpp"
 using namespace std;
 
-template <class E>
-void log(const E& e){cout<<e<<endl;}
 
 int main(int argc, char** argv)
 {
-    log("hello world");
+    LOG("Hello world");
+    TRACE("trace");
+    DEBUG("debug");
+    INFO("info");
+    WARN("warn");
+    ERROR("error");
+    FATAL("fatal");
     return 0;
+}'>>$PROJ_DIR"/"$MAIN
+
+# create console.hpp
+echo '
+// https://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+#ifndef CONSOLE_H
+#define CONSOLE_H
+#include <iostream>
+#include <sstream>
+#include <chrono>
+#include <ctime>
+#include <string>
+#include <algorithm>
+using namespace std;
+#define SPACE " "
+#define COLON ":"
+#define NEWLINE (*new string("\n"))[0]
+
+string getTime()
+{
+    // Some computation here
+    auto end = chrono::system_clock::now();
+    time_t end_time = chrono::system_clock::to_time_t(end);
+
+    // to string
+    stringstream ss;
+    ss << ctime(&end_time);
+    string ret = "[ " + ss.str();
+    ret.erase(std::remove(ret.begin(), ret.end(), NEWLINE), ret.cend());
+    ret += SPACE;
+    return ret + "] --- ";
 }
 
-'>>$PROJ_DIR"/"$MAIN
+
+// colors
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
+#define YELLOW "\x1B[33m"
+#define BLUE "\x1B[34m"
+#define PURPLE "\x1B[35m"
+#define BLACK "\u001b[41m"
+#define RESET "\u001b[0m"
+
+#define MY_PRINT(arg, color, type) cout << color << getTime() << "["<<__FILE__<<"] " << type << COLON << SPACE << arg << RESET << endl;
+
+// MACROs to use
+#define LOG(arg) cout << arg << endl;
+#define TRACE(arg) MY_PRINT(arg, PURPLE, "TRACE" )
+#define DEBUG(arg) MY_PRINT(arg, GREEN, "DEBUG")
+#define INFO(arg) MY_PRINT(arg, BLUE, "INFO")
+#define WARN(arg) MY_PRINT(arg, YELLOW, "WARN")
+#define ERROR(arg) MY_PRINT(arg, RED, "ERROR")
+#define FATAL(arg) MY_PRINT(arg, BLACK, "FATAL")
+
+#endif
+
+'>>$PROJ_DIR"/console.hpp"
 
 # create vscode debuger
 VSCODE="$PROJ_DIR/.vscode"
